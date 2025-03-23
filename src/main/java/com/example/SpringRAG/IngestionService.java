90abfeb61d269e8b0b54d61733d652472ac97690
markussnellman <markus.snellman@gmail.com>
2025-03-23 14:11:43 +0100

    Fix typo in applications.properties

--- src/main/java/com/example/SpringRAG/IngestionService.java
+++ src/main/java/com/example/SpringRAG/IngestionService.java
@@ -2,6 +2,7 @@ package com.example.SpringRAG;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.pdf.ParagraphPdfDocumentReader;
 import org.springframework.ai.transformer.splitter.TextSplitter;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
@@ -10,6 +11,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
 
 @Component
 public class IngestionService implements CommandLineRunner {
@@ -27,7 +31,15 @@ public class IngestionService implements CommandLineRunner {
         var pdfReader = new ParagraphPdfDocumentReader(myDoc);
         TextSplitter textSplitter = new TokenTextSplitter();
 
-        vectorStore.accept(textSplitter.apply(pdfReader.get()));
+        List<Document> splitDocumentChunks = textSplitter.apply(pdfReader.get());
+
+//        List<Document> documents = List.of(
+//                new Document("Spring AI rocks!! Spring AI rocks!! Spring AI rocks!! Spring AI rocks!! Spring AI rocks!!", Map.of("meta1", "meta1")),
+//                new Document("The World is Big and Salvation Lurks Around the Corner"),
+//                new Document("You walk forward facing the past and you turn back toward the future.", Map.of("meta2", "meta2")));
+
+
+        vectorStore.add(splitDocumentChunks);
         log.info("VectorStore loaded with data.");
     }
 }
